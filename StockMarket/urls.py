fc60685dffdd933444a811b4f8ff89b35687a1b7
TanviVejani <tanuvejani05@gmail.com>
2024-10-04 14:41:34 +0530

    First Commit

---
+++ StockMarket/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns=[
+    path('',views.index,name='index'),
+    path('details/',views.details,name='details'),
+    path('compare/',views.compare,name='compare'),
+    path('download/<id>',views.download,name='download'),
+    path('predict/',views.predict,name='predict'),
+    path('company-search/', views.company_search, name='company-search'),
+    path('company-suggest/', views.get_companies, name='get_company_info'),
+    path('company-details/', views.get_company, name='get_company_info'),
+]
